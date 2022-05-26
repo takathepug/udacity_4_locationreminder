1026a6b6d39f0adcf92bc1faef3c87ae06788d96
Esteban Sánchez <essoler@gmail.com>
2022-05-26 15:05:58 +0200

    no-reminders test working

--- app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,29 +1,83 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import android.content.Context
 import android.os.Bundle
+import com.udacity.project4.R
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.data.local.LocalDB
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.GlobalContext.get
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import androidx.test.espresso.matcher.ViewMatchers.*
+import kotlinx.coroutines.test.runBlockingTest
+import org.koin.core.context.GlobalContext
+import org.koin.test.get
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
 class ReminderListFragmentTest {
+    private lateinit var repository: ReminderDataSource
+
+    // executes each task synchronously
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun initRepository() {
+        stopKoin()
+
+        // koin config
+        val myModule = module {
+            viewModel {
+                RemindersListViewModel(
+                    getApplicationContext(),
+                    get() as ReminderDataSource
+                )
+            }
+
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { LocalDB.createRemindersDao(getApplicationContext()) }
+        }
+
+        startKoin {
+            androidContext(getApplicationContext())
+            modules(listOf(myModule))
+        }
+
+        repository = get().koin.get()
+
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+
+    }
+
+    @Test
+    fun noRemindersNoDataMsg() = runBlockingTest {
+        launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+    }
 
 //    TODO: test the navigation of the fragments.
 //    TODO: test the displayed data on the UI.
